f0ba0c75a02a889d8c71becee08833163bf00e5c
exopole <alexandre.nicaise28@gmail.com>
2016-04-04 10:32:33 +0200

    remise a niveau 2

---
+++ Developpement/Noiroroo/src/fenetre/Fenetre.java
@@ -0,0 +1,122 @@
+package fenetre;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.DisplayMode;
+import java.awt.Graphics;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import panneau.Background;
+import panneau.PanneauClass;
+import panneau.PanneauCompetence;
+import panneau.PanneauPersonnage;
+import panneau.PanneauRace;
+
+public class Fenetre extends JFrame{
+	//Background pan = new Background("Portraits/ArmandeConcept.png"); 
+	Background pan;
+	
+	 GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+	public Fenetre() {
+		ImageIcon icon = createImageIcon("images/middle.gif",
+                "a pretty but meaningless splat");
+		JLabel img = new JLabel(icon);
+		
+		// TODO Auto-generated constructor stub
+		this.setTitle("Noiroroo");
+	
+		setMaximized(true);
+		this.setUndecorated(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		int width = tk.getScreenSize().width;
+		int heigth = tk.getScreenSize().height;
+
+		//this.getContentPane().setBackgroundImage(new Background("Portraits/ArmandeConcept.png"));
+		
+		pan = new Background(new Dimension(width, heigth));
+	
+		PanneauPersonnage personnage = new PanneauPersonnage("Portraits/ArmandeConcept.png",new Dimension((width/3)-1, -1+heigth) );
+		PanneauRace race = new PanneauRace("Portraits/ArmandeConcept.png",new Dimension(-1+width/3,-1+2*heigth/3), false );
+		PanneauClass classe = new PanneauClass("Portraits/ArmandeConcept.png",new Dimension(-1+width/3,-1+ 2*heigth/3) );
+		PanneauCompetence competence = new PanneauCompetence("Portraits/ArmandeConcept.png",new Dimension(-1+2*width/3,-1+ heigth/3) );
+
+		
+		
+		pan.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+
+
+		gbc.gridheight = 3;
+		gbc.gridwidth = 1;
+		pan.add(personnage, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridheight = 2;
+		pan.add(race, gbc);
+		
+		gbc.gridx = 2;
+		pan.add(classe, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 2;
+		pan.add(competence, gbc);
+		
+		
+		
+//		
+//		
+//		pan.add(new Portrait("Portraits/ArmandeConcept.png"));
+//		pan.add(new Portrait("Portraits/ArmandeConcept.png"));
+//		pan.add(new Portrait("Portraits/ArmandeConcept.png"));
+//		
+		this.getContentPane().add(pan);
+		this.pack();
+		 this.setVisible(true);
+	}
+	
+	public void setMaximized(boolean maximized){
+	    if(maximized){
+	        DisplayMode mode = this.getGraphicsConfiguration().getDevice().getDisplayMode();
+	        Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration());
+	        this.setMaximizedBounds(new Rectangle(
+	                mode.getWidth() - insets.right - insets.left, 
+	                mode.getHeight() - insets.top - insets.bottom
+	        ));
+	        this.setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+	    }else{
+	        this.setExtendedState(JFrame.NORMAL);
+	    }
+	}
+	
+	/** Returns an ImageIcon, or null if the path was invalid. */
+	protected ImageIcon createImageIcon(String path,
+	                                           String description) {
+	    if (path != null) {
+	        return new ImageIcon(path, description);
+	    } else {
+	        System.err.println("Couldn't find file: " + path);
+	        return null;
+	    }
+	}
+}
---
+++ Developpement/Noiroroo/src/panneau/Background.java
@@ -0,0 +1,32 @@
+package panneau;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class Background extends JPanel{
+	String pathImage;
+	Dimension dim;
+	int width;
+	int height;
+	
+	public Background(Dimension dim) {
+		// TODO Auto-generated constructor stub
+		this.dim = dim;
+		this.setPreferredSize(dim);
+		width = (int) dim.getWidth();
+		height =(int) dim.getHeight();
+		
+		
+	}
+	public void paintComponent(Graphics g){
+		g.drawLine(width/3, 0, width/3, 2*height/3);
+		g.drawLine(2*width/3, 0, 2*width/3, 2*height/3);
+		g.drawLine(0, 2*height/3, width, 2*height/3);
+		
+	}    
+}
